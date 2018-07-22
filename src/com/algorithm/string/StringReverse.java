17554f853896a9a695d1809c83a5f8709861f662
Karthick Sundaram <karthicksun@FDLMC1886.local>
2018-07-22 13:13:53 +0530

    added swap replacement method

--- src/com/algorithm/string/StringReverse.java
+++ src/com/algorithm/string/StringReverse.java
@@ -23,10 +23,29 @@ public class StringReverse {
         reverseStr = reverse(str);
         System.out.println("Reverse String in Java using Iteration: " + reverseStr);
 
+//iterative method to reverse String in Java
+        reverseStr = reverseswap(str);
+        System.out.println("Reverse String in Java using swap Iteration: " + reverseStr);
+
 //recursive method to reverse String in Java
         reverseStr = reverseRecursively(str);
         System.out.println("Reverse String in Java using Recursion: " + reverseStr);
 
+    }
+
+    public static String reverseswap(String str){
+        //StringBuilder strBuilder = new StringBuilder();
+
+        char[] strChars = str.toCharArray();
+
+        for(int i=0; i<strChars.length/2;i++){
+
+            char currentChar = strChars[i];
+            strChars[i] = strChars[strChars.length -1 - i];
+            strChars[strChars.length -1 - i] = currentChar;
+        }
+
+        return new String(strChars);
     }
 
     public static String reverse(String str) {
